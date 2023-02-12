ce48211c482ed17a64a7997c209fa437a65aed9d
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-02-12 18:52:47 -0300

    teste contador regressivo funcionando

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -2,18 +2,25 @@ function comecarContador(duracao, display){
 
     var timer = duracao, minutos, segundos;
 
-    setInterval(decrescerContador(),1000)
-}
-
-function decrescerContador (){
-    minutos = parseInt(timer / 60, 10);
-    segundos = parseInt(timer % 60, 10);
-
-    minutos = minutos < 10 ? "0" + minutos : minutos;
-
-    display.textContent = minutos + ":" + segundos;
-
-    if(--timer < 0){
-        timer = duracao;
+    setInterval( function (){
+        minutos = parseInt(timer / 60, 10);
+        segundos = parseInt(timer % 60, 10);
+    
+        minutos = minutos < 10 ? "0" + minutos : minutos;
+    
+        display.textContent = minutos + ":" + segundos;
+    
+        if(--timer < 0){
+            timer = duracao;
+        }
+        
+        	},1000)
     }
+
+
+window.onload = function(){
+    var duracao = 60*2;
+    var display = document.querySelector('#tempo');
+
+    comecarContador(duracao, display);
 }
