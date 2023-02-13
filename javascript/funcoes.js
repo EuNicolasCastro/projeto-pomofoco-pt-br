e2e657a0914dad43c41b4b189dd55cd5a6d39516
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-02-13 16:19:47 -0300

    javascript do botao para iniciar

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,4 +1,12 @@
-function comecarContador(duracao, display){
+
+let botaoIniciarPausar = document.querySelector("#botao-iniciar-pausar")
+let display = document.querySelector('#tempo');
+
+display.textContent = "25:00"
+
+botaoIniciarPausar.addEventListener("click", iniciarContador);
+
+function rodarContador(duracao, display){
 
     var timer = duracao, minutos, segundos;
 
@@ -20,9 +28,7 @@ function comecarContador(duracao, display){
     }
 
 
-window.onload = function(){
-    var duracao = 10;
-    var display = document.querySelector('#tempo');
-
-    comecarContador(duracao, display);
+function iniciarContador(){
+    var duracao = 60*25;
+    rodarContador(duracao, display);
 }
