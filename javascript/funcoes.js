04a7238f34ffc3d020b3d59ef8d50c4aaa26c33b
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-02-14 12:29:38 -0300

    organizando a pagina sobre.html

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -3,12 +3,18 @@ let botaoIniciarPausar = document.querySelector("#botao-iniciar-pausar")
 let display = document.querySelector('#tempo');
 
 display.textContent = "25:00"
-
+let iniciou = false;
 botaoIniciarPausar.addEventListener("click", iniciarContador);
+
+function iniciarContador(){
+    var duracao = 60*25;
+
+    rodarContador(duracao, display);
+}
 
 function rodarContador(duracao, display){
 
-    var timer = duracao, minutos, segundos;
+    let timer = duracao, minutos, segundos;
 
     setInterval( function (){
         minutos = parseInt(timer / 60, 10);
@@ -18,7 +24,7 @@ function rodarContador(duracao, display){
         segundos = segundos <10 ? "0"+ segundos : segundos;
     
         display.textContent = minutos + ":" + segundos;
-    
+
         if(--timer < 0){
             timer = 0;
             display.textContent = "ACABOU"
@@ -28,7 +34,6 @@ function rodarContador(duracao, display){
     }
 
 
-function iniciarContador(){
-    var duracao = 60*25;
-    rodarContador(duracao, display);
+function pausarContador(timer){
+   display.textContent = timer;
 }
