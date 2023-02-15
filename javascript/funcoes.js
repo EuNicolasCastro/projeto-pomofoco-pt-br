f023589c460d0c8b654595f8fb21ce9295828fce
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-02-15 15:52:26 -0300

    TENTADO ZERAR O CRONOMETRO

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,39 +1,53 @@
 
-let botaoIniciarPausar = document.querySelector("#botao-iniciar-pausar")
+let botaoIniciarPausar = document.querySelector("#botao-iniciar-pausar");
+let botaoZerar = document.querySelector("#botao-zerar");
 let display = document.querySelector('#tempo');
 
 display.textContent = "25:00"
 let iniciou = false;
 botaoIniciarPausar.addEventListener("click", iniciarContador);
+botaoZerar.addEventListener("click", zerarContador);
+
 
 function iniciarContador(){
-    var duracao = 60*25;
-
+    var duracao = 60*24 + 59;
+    iniciou=true;
     rodarContador(duracao, display);
 }
 
 function rodarContador(duracao, display){
 
     let timer = duracao, minutos, segundos;
+    while (iniciou) {
+        setInterval( function (){
+            
+                minutos = parseInt(timer / 60, 10);
+                segundos = parseInt(timer % 60, 10);
+            
+                minutos = minutos < 10 ? "0" + minutos : minutos;
+                segundos = segundos <10 ? "0"+ segundos : segundos;
+            
+                display.textContent = minutos + ":" + segundos;
 
-    setInterval( function (){
-        minutos = parseInt(timer / 60, 10);
-        segundos = parseInt(timer % 60, 10);
-    
-        minutos = minutos < 10 ? "0" + minutos : minutos;
-        segundos = segundos <10 ? "0"+ segundos : segundos;
-    
-        display.textContent = minutos + ":" + segundos;
-
-        if(--timer < 0){
-            timer = 0;
-            display.textContent = "ACABOU"
+                if(--timer < 0){
+                    timer = 0;
+                    display.textContent = "ACABOU"
+                }   
+            
+                },1000)
         }
-        
-        	},1000)
     }
 
 
 function pausarContador(timer){
    display.textContent = timer;
+}
+
+function zerarContador(){
+    iniciou = false;
+    display.textContent = "25:00"
+}
+
+function checarIniciou(){
+    
 }
